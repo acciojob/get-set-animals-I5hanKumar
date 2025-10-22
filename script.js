592715a883ba0bfa8d04d79d7c932c695ecd45e3
I5hanKumar <153290701+I5hanKumar@users.noreply.github.com>
2025-10-22 17:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-//complete this code
 class Animal {
-	species : string;
-	
-	constructor(species :string );
-	this.species = species;
+
+	constructor(species){
+		this.species = species;
+}
 
 get new_species(){
 	return this.new_species = species;
 }
 
 makeSound(){
-	console.log("The" + this.species + "make a sound");
+	console.log("The" + this.species + "makes a sound");
 }
 
 }
 class Dog extends Animal {
-	
-    const myDog = new Dog("Golden Retriever");
 	bark(){console.log("woof")};
 }
+  const myDog = new Dog("Golden Retriever");
+
 class Cat extends Animal {
-	const mycat = new Cat("Siamese");
 	purr(){console.log("purr")};
 }
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+const mycat = new Cat("Siamese");
